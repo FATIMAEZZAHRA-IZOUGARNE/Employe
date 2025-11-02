75e816234898cad70fdeb26aec5e9c869ff52006
FATIMAEZZAHRA-IZOUGARNE <fatimaezzahraizougarne@gmail.com>
2025-11-02 23:25:20 +0100

    Restore ConsoleApplication6.cpp content

--- ConsoleApplication6.cpp
+++ ConsoleApplication6.cpp
@@ -40,7 +40,7 @@ int main()
         cout << "\n--- Informations du commercial ---\n";
         c1.afficher();
 
-        cout << "\nProgramme terminé avec succès ✅\n";
+        cout << "\nProgramme terminé avec succès \n";
 
         return 0;
 
@@ -59,3 +59,4 @@ int main()
 //   4. Use the Error List window to view errors
 //   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
 //   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
+
